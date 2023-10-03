d16771aed8150eae0d44eb9ec4c0947c0af97de5
Milena Alves <87149523+aWhiity@users.noreply.github.com>
2023-10-03 16:16:03 -0300

    att

--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -1,70 +1,34 @@
 package entities;
 
 public class Aluno {
-	private int RA;
+
+	private int ra;
 	private String nome;
 	private String sexo;
 	private String curso;
-	private String dataIngresso;
-	private int periodo;
-	private double coeficiente;
 	
-	public int getRA() {
-		return RA;
+	public int getRa() {
+		return ra;
 	}
-	
-	public void setRA(int rA) {
-		RA = rA;
+	public void setRa(int ra) {
+		this.ra = ra;
 	}
-	
 	public String getNome() {
 		return nome;
 	}
-	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
 	public String getSexo() {
 		return sexo;
 	}
-	
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-	
 	public String getCurso() {
 		return curso;
 	}
-	
 	public void setCurso(String curso) {
 		this.curso = curso;
-	}
-	
-	public String getDataIngresso() {
-		return dataIngresso;
-	}
-	
-	public void setDataIngresso(String dataIngresso) {
-		this.dataIngresso = dataIngresso;
-	}
-	
-	public int getPeriodo() {
-		return periodo;
-	}
-	
-	public void setPeriodo(int periodo) {
-		this.periodo = periodo;
-	}
-	
-	public double getCoeficiente() {
-		return coeficiente;
-	}
-	
-	public void setCoeficiente(double coeficiente) {
-		this.coeficiente = coeficiente;
-	}
-	
-	
-	
+	}	
 }
